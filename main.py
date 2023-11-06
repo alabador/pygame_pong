6719edfaf00aeba75ca95556b14396211fff8679
Austin <austinlabador@gmail.com>
2023-11-06 00:06:51 -0800

    Add player input to player class

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Any
 import pygame
 import sys
 import random
@@ -8,14 +9,43 @@ display_w, display_h = 800, 400
 display = pygame.display.set_mode((display_w, display_h))
 clock = pygame.time.Clock()
 
-class Player:
-    def __init__(self) -> None:
-        pass
+class Player(pygame.sprite.Sprite):
+    def __init__(self, is_player) -> None:
+        super().__init__()
+        if is_player:
+            self.paddle = pygame.Rect(10, 10, 10, 70)
+            self.player = True
+        else:
+            self.paddle = pygame.Rect(display_w - 20, 10, 10, 70)
+            self.player = False
+    def draw_paddle(self):
+        pygame.draw.rect(display, 'WHITE', self.paddle)
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if self.player:
+            if keys[pygame.K_w]:
+                self.paddle.y -= 5
+            if keys[pygame.K_s]:
+                self.paddle.y += 5
+        else:
+            if keys[pygame.K_UP]:
+                self.paddle.y -= 5
+            if keys[pygame.K_DOWN]:
+                self.paddle.y += 5
+    def check_collision(self):
+        if self.paddle.top <= 10:
+            self.paddle.top = 10
+        elif self.paddle.bottom >= display_h - 10:
+            self.paddle.bottom = display_h - 10
+    def update(self) -> None:
+        self.player_input()
+        self.check_collision()
 
 
 def draw_bg():
-    offwhite = (230,230,230)
-    border = pygame.draw.rect(display, offwhite, (0, 0, display_w, display_h), 10)
+    offwhite = (207, 59, 29)
+    border_top = pygame.draw.line(display, offwhite, (0,0), (display_w,0), 15)
+    border_bottom = pygame.draw.line(display, offwhite, (0, display_h), (display_w, display_h), 15)
     divider = pygame.draw.rect(display, offwhite, (display_w/2, 0, 10, display_h))
 
 def render_menu():
@@ -37,7 +67,8 @@ def render_menu():
     
 
 
-
+player = Player(True)
+opp = Player(False)
 
 game_active = False
  
@@ -52,6 +83,10 @@ while True:
     if game_active == True:
         display.fill((0,0,0))
         draw_bg()
+        player.draw_paddle()
+        player.update()
+        opp.draw_paddle()
+        opp.update()
     else:
         game_active = render_menu()
     pygame.display.update()
