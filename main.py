edd7ffa223647ff7be52c41f70b0d858c27e4356
Austin <austinlabador@gmail.com>
2023-11-06 16:00:56 -0800

    Add goal detection and score iteration

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, is_player) -> None:
         super().__init__()
         self.y = 10
+        self.score = 0
         if is_player:
             self.x = 10
             self.paddle = pygame.Rect(self.x, self.y, 10, 70)
@@ -27,9 +28,9 @@ class Player(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
         if self.player:
             if keys[pygame.K_w]:
-                self.paddle.y -= 5
+                self.paddle.y -= 10
             if keys[pygame.K_s]:
-                self.paddle.y += 5
+                self.paddle.y += 10
         else:
             if keys[pygame.K_UP]:
                 self.paddle.y -= 5
@@ -50,22 +51,48 @@ class Ball(pygame.sprite.Sprite):
         super().__init__()
         self.x = display_w/2
         self.y = display_h/2
-        self.x_movement = 5
-        self.y_movement = 5
+        self.x_movement = 8
+        self.y_movement = random.randint(-10, 10)
         self.radius = 10
         self.object = pygame.draw.circle(display, (66, 214, 255), (self.x, self.y), self.radius)
     def draw_ball(self):
         self.object = pygame.draw.circle(display, (66, 214, 255), (self.x, self.y), self.radius)
     def movement(self):
         self.x += self.x_movement
+        self.y += self.y_movement
     def check_collision(self):
         if self.object.colliderect(player.paddle):
             self.x_movement = -self.x_movement
-        elif self.object.colliderect(opp.paddle):
+        if self.object.colliderect(opp.paddle):
             self.x_movement = -self.x_movement
+        if self.object.y <= 10:
+            self.y_movement = -self.y_movement
+        if self.object.y >= display_h - 20:
+            self.y_movement = -self.y_movement
+    def reset(self):
+        self.x = display_w/2
+        self.y = display_h/2
+        self.x_movement = 0
+        self.y_movement = 0
+    def restart_movement(self):
+        self.y_movement = random.randint(-10, 10)
+        self.x_movement = 8
+    def check_win(self):
+        if self.object.x > display_w + self.radius:
+            player.score += 1
+            self.reset()
+        if self.object.x < 0 - self.radius:
+            opp.score += 1
+            self.reset()
+        # keys = pygame.key.get_pressed()
+        # if keys[pygame.K_SPACE]:
+        #     self.y_movement = random.randint(-10, 10)
+        #     self.x_movement = 8
+        
     def update(self) -> None:
         self.draw_ball()
         self.check_collision()
+        self.check_win()
         self.movement()
 
 
@@ -92,7 +119,16 @@ def render_menu():
     if keys[pygame.K_SPACE]:
         return True
     
+def score_board():
+    score_font = pygame.font.Font('fonts/PixelifySans-VariableFont_wght.ttf', 24)
+    player_score_text = score_font.render(f"Player: {player.score}", False, "WHITE")
+    player_score_rect = player_score_text.get_rect(center = (display_w/4, 350)) 
+    cpu_score_text = score_font.render(f"CPU: {opp.score}", False, "WHITE")
+    cpu_score_rect = cpu_score_text.get_rect(center = (display_w * 3/4, 350)) 
 
+    display.blit(player_score_text, player_score_rect)
+    display.blit(cpu_score_text, cpu_score_rect)
+    
 
 player = Player(True)
 opp = Player(False)
@@ -108,12 +144,17 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 game_active = False
+                player.score = 0
+                opp.score = 0
+            if event.key == pygame.K_SPACE:
+                ball.restart_movement()
     if game_active == True:
         display.fill((0,0,0))
         draw_bg()
         player.update()
         opp.update()
         ball.update()
+        score_board()
     else:
         game_active = render_menu()
     pygame.display.update()
